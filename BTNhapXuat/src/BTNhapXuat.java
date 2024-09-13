b7c9bf7b8255e9506c373e1b9521c47b5755b2fb
datx24 <xuandat475@gmail.com>
2024-09-13 14:08:30 +0700

    thêm comment trong code

--- BTNhapXuat/src/BTNhapXuat.java
+++ BTNhapXuat/src/BTNhapXuat.java
@@ -4,13 +4,19 @@ public class BTNhapXuat {
 	public static void main(String[] args) {
 		//Khai báo biến
 		int a,b,tong;
+		
+		//Chuẩn bị nhập dữ liệu
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Nhập a: ");
+		
 		//Nhập dữ liệu
+		System.out.println("---- CHƯƠNG TRÌNH TÍNH TỔNG HAI SỐ NGUYÊN ----");
+		System.out.print("Nhập a: ");
 		a = scanner.nextInt();
 		System.out.print("Nhập b: ");
 		b = scanner.nextInt();
 		tong = a + b;
+		
+		//Xuất kết quả
 		System.out.print("Tổng hai số a và b: " +tong);
 	}
 }
