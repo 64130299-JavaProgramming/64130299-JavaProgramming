f9361d65e58894a39cf4d54411f088f732c89baf
datx24 <xuandat475@gmail.com>
2024-10-22 16:19:49 +0700

    kết nối tới my sql comment haha

--- DemoBanHang/src/main/java/Home.java
+++ DemoBanHang/src/main/java/Home.java
@@ -1,7 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Home {
-	public static void main(String[] args) {
-		System.out.println("Hello");
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//Đăng kí divew
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		String strConn = "jdbc:mysql://localhost:3306/demobanhang" ;
+		
+		Connection conn = DriverManager.getConnection(strConn, "root", "");
+		System.out.println("Haha");
 	}
 
 }
