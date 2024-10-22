6fe95a1d49773806ed466b0243d349af9e03f9d6
datx24 <xuandat475@gmail.com>
2024-10-22 16:32:14 +0700

    đọc dữ liệu mysql

--- DemoBanHang/src/main/java/Home.java
+++ DemoBanHang/src/main/java/Home.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Home {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -10,7 +12,19 @@ public class Home {
 		String strConn = "jdbc:mysql://localhost:3306/demobanhang" ;
 		
 		Connection conn = DriverManager.getConnection(strConn, "root", "");
-		System.out.println("Haha");
+		
+		String sqlSelect = "Select * from SanPham";
+		Statement lenh = conn.createStatement();
+		ResultSet ketQua = lenh.executeQuery(sqlSelect);
+		
+		//Hiện kết quả
+		while (ketQua.next()) {
+			int id = ketQua.getInt(1); // lấy dữ liệu ở cột 0
+			String tenSP = ketQua.getString(2);
+			String giaSP = ketQua.getString(3);
+			String moTa = ketQua.getString(4);
+			System.out.println(id + " | " + tenSP + " | " + giaSP + " | " + moTa + "\n");
+		}
 	}
 
 }
