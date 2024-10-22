b69299ba3bbdca0ba313b5a1efa0c82100676a28
datx24 <xuandat475@gmail.com>
2024-10-23 06:36:14 +0700

    Kết nối đến csdl và load sản phảm từ csdl trong controller

--- fxml_QuanLyBanHang/src/main/java/fxml_QuanLyBanHang/MainController.java
+++ fxml_QuanLyBanHang/src/main/java/fxml_QuanLyBanHang/MainController.java
@@ -1,5 +1,46 @@
 package fxml_QuanLyBanHang;
 
-public class MainController {
 
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MainController {
+	//Khai báo các thuộc tính cần sử dụng
+	private TextField idField;
+	private TextField nameField;
+	private TextField priceField;
+	private TextField descriptionField;
+	private TableView<SanPham> productTable;
+	
+	//Khai báo các cột trong tableview
+	private TableColumn<SanPham, Integer> idColumn;
+	private TableColumn<SanPham, String> nameColumn;
+	private TableColumn<SanPham, Double> priceColumn;
+	private TableColumn<SanPham, String> descriptionColumn;
+	
+	//Khai báo các nút xử lý
+	private Button addButton;
+	private Button deleteButton;
+	private Button editButton;
+	private Button updateButton;
+	private Button seacrhButton;
+	
+	//Khai báo biến để kết nối cơ sở dữ liệu
+	private Connection connection;
+	// Danh sách chứa các sản phẩm, cho phép theo dõi sự thay đổi trong danh sách.
+	// Khi danh sách thay đổi, giao diện người dùng có thể tự động cập nhật.
+	private ObservableList<SanPham> productList;
+	
+	private void connectToDatabase() throws SQLException {
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demobanhang","root", "");
+		System.out.println("Kết nối cơ sở dữ liệu thành công !");
+	}
 }
--- fxml_QuanLyBanHang/src/main/java/fxml_QuanLyBanHang/SanPham.java
+++ fxml_QuanLyBanHang/src/main/java/fxml_QuanLyBanHang/SanPham.java
@@ -1,5 +1,52 @@
 package fxml_QuanLyBanHang;
 
-public class SanPham {
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerPropertyBase;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
+public class SanPham {
+	private final IntegerPropertyBase id;
+	private final StringProperty name;
+	private final DoubleProperty price;
+	private final StringProperty description;
+	public SanPham(int id, String name, double price, String description) {
+		super();
+		this.id = new SimpleIntegerProperty(id);
+		this.name = new SimpleStringProperty(name);
+		this.price = new SimpleDoubleProperty(price);
+		this.description = new SimpleStringProperty(description);
+	}
+	
+	public int getId() {
+		return id.get();
+	}
+	public IntegerPropertyBase idProperty() {
+		return id;
+	}
+	
+	public String getName() {
+		return name.get();
+	}
+	public StringProperty nameProperty() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price.get();
+	}
+	public DoubleProperty priceProperty() {
+		return price;
+	}
+	
+	public String getDescription() {
+		return description.get();
+	}
+	public StringProperty descriptionProperty() {
+		return description;
+	}
+	
+	
 }
