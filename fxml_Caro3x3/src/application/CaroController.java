96d3acf30243becb26adf0f12f10f694ef86c5f6
datx24 <xuandat475@gmail.com>
2024-10-24 21:58:31 +0700

    Khởi tạo bàn cờ trống

--- fxml_Caro3x3/src/application/CaroController.java
+++ fxml_Caro3x3/src/application/CaroController.java
@@ -1,5 +1,23 @@
 package application;
 
-public class CaroController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class CaroController {
+	@FXML
+	private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+	//Biến để theo dõi lượt của người chơi
+	private boolean isXTurn = true;
+	//Mangr lưu trạng thái của bàn cờ
+	private String[][] board = new String[3][3];
+	
+	@FXML
+	public void initialize() {
+		//Khởi tạo bàn cờ trống
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				board[i][j] = "";
+			}
+		}
+	}
 }
