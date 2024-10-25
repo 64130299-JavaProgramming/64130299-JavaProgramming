f81b95b351f293acdee3d77cccefa7f1cfdbfefb
datx24 <xuandat475@gmail.com>
2024-10-25 21:14:30 +0700

    code và chạy giao diện app tính toán

---
+++ AppTinhToanGK/src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		//Tải file xml
+		Parent root = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
+		primaryStage.setTitle("App Tính Toán Cơ Bản");
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
