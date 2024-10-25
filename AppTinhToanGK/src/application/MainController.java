3ef229df5dfa56e1b48b00e399b8c41431dacfaa
datx24 <xuandat475@gmail.com>
2024-10-25 21:28:07 +0700

    đặt tên biến

--- AppTinhToanGK/src/application/MainController.java
+++ AppTinhToanGK/src/application/MainController.java
@@ -1,5 +1,13 @@
 package application;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
+public class MainController {
+	@FXML
+	private TextField txtKetQua;
+	
+	private StringBuilder duLieuHienTai = new StringBuilder();
+	private double ketQua = 0;
+	private String toanTuCuoi = "=";
 }
