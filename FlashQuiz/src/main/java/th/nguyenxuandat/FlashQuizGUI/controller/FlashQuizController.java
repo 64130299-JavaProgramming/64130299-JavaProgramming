43ff34e4d1a5d4053d6dd9f368a676cbac40607e
datx24 <xuandat475@gmail.com>
2024-12-06 23:46:41 +0700

    cài đặt đổi màu khi bấm button

--- FlashQuiz/src/main/java/th/nguyenxuandat/FlashQuizGUI/controller/FlashQuizController.java
+++ FlashQuiz/src/main/java/th/nguyenxuandat/FlashQuizGUI/controller/FlashQuizController.java
@@ -1,5 +1,41 @@
 package th.nguyenxuandat.FlashQuizGUI.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlashQuizController {
 
+    @FXML private Button btnQuestion1,btnQuestion2,btnQuestion3,btnQuestion4,btnQuestion5,
+    btnQuestion6,btnQuestion7,btnQuestion8,btnQuestion9,btnQuestion10;
+
+    private List<Button> buttonList; // Danh sách các button để thực hiển đổi màu
+
+    @FXML
+    public void initialize() {
+        initializeButtonList();
+    }
+
+     //Hàm thêm tất cả các button vào danh sách để xử lý đổi màu
+    private void initializeButtonList() {
+        buttonList = new ArrayList<>();
+
+        // Lấy tất cả các button và thêm vào danh sách
+        buttonList.addAll(List.of(btnQuestion1, btnQuestion2, btnQuestion3, btnQuestion4, btnQuestion5, 
+        		btnQuestion6, btnQuestion7, btnQuestion8, btnQuestion9, btnQuestion10));
+    }
+
+    @FXML
+    public void handleButtonClick(javafx.event.ActionEvent event) {
+        // Đưa tất cả button về màu mặc định
+        for (Button btn : buttonList) {
+            btn.setStyle("-fx-background-color: #D4EBF8;");
+        }
+
+        // Đổi màu cho button được nhấn
+        Button clickedButton = (Button) event.getSource();
+        clickedButton.setStyle("-fx-background-color: #F96E2A;");
+    }
 }
+
